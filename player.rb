20ef61d75d94066b548a54a9417e1f4dd7ef9ec9
Taejin Jung <taejin5314@gmail.com>
2021-08-16 12:11:31 -0400

    add main file and player class

---
+++ player.rb
@@ -0,0 +1,24 @@
+class Player
+  attr_accessor :name, :lives
+  LIVES = 3
+
+  def initialize(name)
+    @name = name
+    @lives = LIVES
+    @short_name = "P#{name.split.last}"
+  end
+
+  def info
+    "#{short_name}: #{lives}/#{LIVES}"
+  end
+
+  def lose
+    self.lives--
+  end
+
+  def win_message
+    "#{name} wins with a score of #{lives}/#{LIVES}"
+  end
+end
+
+
